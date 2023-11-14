e9d494a3fbb10a7941983f8a19c7e9ac48769c30
clay <dcheng69@uwo.ca>
2023-11-14 16:56:04 -0500

    Add fake fetch data for web client test
    [Ticket: feature: GUI client]
    
    [body]
    1. add fake fetch data for web client test
    
    [test]
    1. test locally

--- client_web/web_client.py
+++ client_web/web_client.py
@@ -18,6 +18,14 @@ def search():
 
     if response.status_code == 200:
         data = response.json()
+        # Add fake json data
+        fake_data = [
+                {"service_id": 101, "title": "stock overtime performance check", "ip": "127.0.0.1", "port":"5009"},
+                {"service_id": 102, "title": "stock notifications", "ip": "127.0.0.1", "port":"5010"},
+        ]
+
+        # Uncomment the line below to use fake data for testing
+        return render_template('search_results.html', results=fake_data)
         return render_template('search_results.html', results=data)
     else:
         return "Error fetching data from the registry server!"
